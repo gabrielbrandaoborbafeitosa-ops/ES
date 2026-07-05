d3253bbd749e2835a70d5c5e1576ad2e4d316e25
gabrielbrandaoborbafeitosa-ops <gabrielbrandaoborbafeitosa@gmail.com>
2026-07-05 10:14:32 -0300

    Primeira versão
    Build 1

---
+++ script.js
@@ -0,0 +1,19 @@
+function executar() {
+    let comando = document.getElementById('inputcomando').value ;
+    let valor = document.getElementById('inputvalor').value ;
+    let saida = document.getElementById('txtsaida')
+    //procurar comando
+if (comando == 'cowspeak'){
+    saida.innerText = '🐮<|' + valor + '|' ;
+} else if(comando == 'calc') {
+    let calculo = Number(valor)
+    saida.innerText = calculo ;
+}  else if(comando == 'text') {
+    saida.innerText = valor;
+} else if(comando == 'open-window') {
+    window.open(valor)
+}
+}
+function explicar() {
+    alert('digite o nome do comando onde está escrito "Comando" e o valor dele em "Valor".\nComandos:\ncomando: cowsapeak valor: (mensagem aqui)\ncomando: calc valor: (Cáuculo aqui) (ATENÇÃO coloque o sinal entre espaços, como 1 + 1, não 1+1)\ncomando: text valor: (texto aqui)\ncomando: open-window valor: (URL)')
+}

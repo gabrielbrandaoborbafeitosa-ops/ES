d5f69fddd125b44b0416554df488025a0e354f50
gabrielbrandaoborbafeitosa-ops <gabrielbrandaoborbafeitosa@gmail.com>
2026-07-17 11:35:57 -0300

    novo JS
    Build 5

--- script.js
+++ script.js
@@ -9,10 +9,14 @@ if (comando == 'cowspeak'){
     saida.innerText = valor;
 } else if(comando == 'open-window') {
     window.open(valor)
+} else if(comando == 'js') {
+    if(confirm('ATENÇÃO\nNÃO use um JavaScript de uma fonte não confiável. Deseja executar?') == true) {
+        eval(valor)
+    }
 } else {
     saida.innerText = 'Spelling error: ' + comando + ': Command not found';
 }
 }
 function explicar() {
-    alert('digite o nome do comando onde está escrito "Comando" e o valor dele em "Valor".\nComandos:\ncomando: cowsapeak valor: (mensagem aqui)\ncomando: text valor: (texto aqui)\ncomando: open-window valor: (URL)')
+    alert('digite o nome do comando onde está escrito "Comando" e o valor dele em "Valor".\nComandos:\ncomando: cowsapeak valor: (mensagem aqui)\ncomando: text valor: (texto aqui)\ncomando: open-window valor: (URL)\nComando: js Valor: (JavaScript aqui)')
 }
